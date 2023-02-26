6aa0ffbbce078c5a6904a1c824864cb13a67c3a0
Ricardo Gonzales <rzamarron20@unm.edu>
2023-02-26 15:16:02 -0700

    LockController

--- src/drivers/LockController.java
+++ src/drivers/LockController.java
@@ -1,36 +1,20 @@
 package drivers;
 
-public class LockController {
+import gui.LockActuator;
+
+public class LockController implements LockActuator {
     /*
-     *A lock interface which is surrounded by calls to lock and
-     * unlock method
+     *code to disengage the lock actuator
      */
-    private boolean isLocked;
-
-    public LockController() {
-        this.isLocked = true;
+private boolean isEngaged = false;
+    @Override
+    public void disengage() {
+        isEngaged = false;
     }
 
-    public void engageLock() {
-        if (this.isLocked) {
-            System.out.println("The lock is already engaged.");
-        } else {
-            this.isLocked = true;
-            System.out.println("The lock has been engaged.");
-        }
-    }
-
-    public void disengageLock() {
-        if (!this.isLocked) {
-            System.out.println("The lock is already disengaged.");
-        } else {
-            this.isLocked = false;
-            System.out.println("The lock has been disengaged.");
-        }
-    }
-
-    public boolean isLocked() {
-        return this.isLocked;
+    @Override
+    public boolean engaged() {
+        return isEngaged;
     }
 }
 
