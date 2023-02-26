5e680edd351e173a9331973f82c4416f9a511996
Ricardo Gonzales <rzamarron20@unm.edu>
2023-02-26 16:07:21 -0700

    LockController fix

--- src/drivers/LockController.java
+++ src/drivers/LockController.java
@@ -1,22 +1,43 @@
 package drivers;
 
 import gui.LockActuator;
+import safe.SafeController;
 
-public class LockController implements LockActuator {
+public class LockController {
+    private LockActuator actuator;
+    private SafeController safeController;
+    private boolean isLocked;
 
-private boolean isEngaged = false;
-/*
- * method sets the isEngaged field to false,
- * indicating that the lock actuator is disengaged.
- */
-    @Override
+    public LockController(LockActuator actuator, SafeController safeController) {
+        this.actuator = actuator;
+        this.safeController = safeController;
+        this.isLocked = true;
+    }
+    /* disengages the lock
+     */
     public void disengage() {
-        isEngaged = false;
+        if (isLocked) {
+            actuator.disengage();
+            isLocked = false;
+        }
+    }
+    /*method is called to lock the lock
+     */
+    public void engage() {
+        if (!isLocked) {
+            actuator.disengage();
+            isLocked = true;
+        }
+    }
+/*
+    public void update() {
+        if (safeController.isUnlocked() && isLocked) {
+            disengage();
+        } else if (safeController.isLocked() && !isLocked) {
+            engage();
+        }
     }
 
-    @Override
-    public boolean engaged() {
-        return isEngaged;
-    }
+ */
 }
 
