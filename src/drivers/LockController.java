361976715fa5037eb243d8c6b7742c3da1ada200
Ricardo Gonzales <rzamarron20@unm.edu>
2023-02-26 14:56:14 -0700

    LockController

--- src/drivers/LockController.java
+++ src/drivers/LockController.java
@@ -1,21 +1,36 @@
 package drivers;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class LockController {
     /*
      *A lock interface which is surrounded by calls to lock and
      * unlock method
      */
-    private static Lock lock = new ReentrantLock();
+    private boolean isLocked;
 
-    public void disengage() {
-        lock.lock();
-        try {
-            System.out.println("LOCKED"); // I will need to implement the logic
-        } finally {
-            lock.unlock();
+    public LockController() {
+        this.isLocked = true;
+    }
+
+    public void engageLock() {
+        if (this.isLocked) {
+            System.out.println("The lock is already engaged.");
+        } else {
+            this.isLocked = true;
+            System.out.println("The lock has been engaged.");
         }
     }
+
+    public void disengageLock() {
+        if (!this.isLocked) {
+            System.out.println("The lock is already disengaged.");
+        } else {
+            this.isLocked = false;
+            System.out.println("The lock has been disengaged.");
+        }
+    }
+
+    public boolean isLocked() {
+        return this.isLocked;
+    }
 }
+
