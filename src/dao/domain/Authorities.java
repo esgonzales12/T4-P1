49b9308f6a98f9dcb3695b1d3fc7e82c84d8834c
Junyi Zheng <122923483+JunyiZheng0724@users.noreply.github.com>
2023-03-01 20:53:33 -0700

    Update Authorities.java

--- src/dao/domain/Authorities.java
+++ src/dao/domain/Authorities.java
@@ -1,13 +1,36 @@
 package dao.domain;
 
 public enum Authorities {
-    ADMIN,
-    USER,
-    GUEST, 
-    SAVE, 
-    DELETE, 
-    GET, 
-    MODIFY;
+    ZERO("0"),
+    ONE("1"),
+    TWO("2"),
+    THREE("3"),
+    FOUR("4"),
+    FIVE("5"),
+    SIX("6"),
+    SEVEN("7"),
+    EIGHT("8"),
+    NINE("9"),
+    A("A"),
+    B("B"),
+    C("C"),
+    D("D"),
+    E("E"),
+    F("F"),
+    PASSWORD_MANAGEMENT("*"),
+    CANCEL("#");
+
+    private final String value;
+
+    Authorities(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
+
 
     public boolean contains(Authorities authority) {
         for (Authorities a : values()) {
