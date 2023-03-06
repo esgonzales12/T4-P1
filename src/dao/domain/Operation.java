77543d22d042d519625b04304feab0378b1550bd
Junyi Zheng <122923483+JunyiZheng0724@users.noreply.github.com>
2023-03-05 20:04:37 -0700

    Update Operation.java

--- src/dao/domain/Operation.java
+++ src/dao/domain/Operation.java
@@ -4,5 +4,36 @@ public enum Operation {
     USER_MANAGEMENT,
     CHANGE_PASSWORD,
     EXPORT_LOGS,
-    BASE_ACCESS
+    BASE_ACCESS,
+    CANCEL;
+
+    public boolean contains(Operation operation) {
+        for (Operation op : Operation.values()) {
+            if (op.equals(operation)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void clear() {
+        // Reset the enum value to its default state
+        switch (this) {
+            case USER_MANAGEMENT:
+                // Reset user management permissions
+                break;
+            case CHANGE_PASSWORD:
+                // Reset change password permissions
+                break;
+            case EXPORT_LOGS:
+                // Reset export logs permissions
+                break;
+            case BASE_ACCESS:
+                // Reset base access permissions
+                break;
+            default:
+                // Do nothing
+                break;
+        }
+    }
 }
